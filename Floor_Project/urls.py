7bb3713caffd0e5fe4b380f43bed253a499fd09c
akash027 <33549683+akash027@users.noreply.github.com>
2020-12-12 16:09:08 +0530

    Update urls.py

--- Floor_Project/urls.py
+++ Floor_Project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+#from django.urls import path
 from django.conf.urls import url
 from floor_detection import views
 from django.conf.urls.static import static
@@ -23,7 +23,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url("^$", views.index, name="homepage"),
     url("predictImage", views.predictImage, name="predictImage")
 ]
