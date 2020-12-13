8f9f0ecbb7f396af7352582ccc731f69e8b3cf80
akash027 <33549683+akash027@users.noreply.github.com>
2020-12-13 14:38:00 +0530

    Update urls.py

--- Floor_Project/urls.py
+++ Floor_Project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from .floor_detection import views
+from floor_detection import views
 from django.conf.urls.static import static
 from django.conf import settings
 
